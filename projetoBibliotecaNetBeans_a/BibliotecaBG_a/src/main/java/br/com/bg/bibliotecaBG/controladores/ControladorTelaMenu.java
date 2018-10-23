f57a1b7e561f0ba55492172ea052c95eb328911c
Douglas Dourado <43358776+angelobr12@users.noreply.github.com>
2018-10-22 22:29:48 -0300

    Add files via upload

---
+++ projetoBibliotecaNetBeans_a/BibliotecaBG_a/src/main/java/br/com/bg/bibliotecaBG/controladores/ControladorTelaMenu.java
@@ -0,0 +1,24 @@
+
+package br.com.bg.bibliotecaBG.controladores;
+
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+/**
+ *
+ * @author 2binfo
+ */
+public class ControladorTelaMenu {
+    
+    private JLabel jlahora;
+    private JLabel jladata;
+    private JLabel jladiadasemana;
+    private Timer timer;
+    
+    
+}
+public ControladorTelaMenu(JFrame tela menu,){
+    super(telaMenu, desktop);
+this.jlaHora = jlaHora;
+this.jlaData = jlaData;
+this.jlaDiaSemana = jlaDiaSemana;
